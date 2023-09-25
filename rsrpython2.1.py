cc74dd6f89c156a0cd493660465c9e065b676f6f
rsrpython <145908536+rsrpython@users.noreply.github.com>
2023-09-25 16:40:06 -0300

    Add files via upload

--- rsrpython2.1.py
+++ rsrpython2.1.py
@@ -1,5 +1,6 @@
 produtos=["arroz","feijão","macarrão","óleo de soja","litro de leite","pão integral","pasta de amendoim","aveia","1kl de bananas","1/2kl de café","1l de yogourt"]
 custos=[4.50,3.75,4.10,5.20,5.60,7.20,6.80,9.10,4.10,6.30,9.20]
+quantidades=[5,2,4,2,6,3,2,2,3,3,4]
 imp1=12
 imp2=6
 imp3=3
@@ -7,6 +8,7 @@ frete=50.00
 for i in range(len(produtos)):
     produto=produtos[i]
     custo=custos[i]
+    quantidade=quantidades[i]
     lucro=60
     custoimp1=((custo*imp1)/100)
     custoimp2=((custo*imp2)/100)
@@ -14,5 +16,6 @@ for i in range(len(produtos)):
     precocusto=(custo+custoimp1+custoimp2+custoimp3)
     margemdelucro=((precocusto*lucro)/100)
     precovenda=(precocusto+margemdelucro)
-    precovendacomfrete=(precovenda+frete)
-    print(f"produto: ", produto, "custo s/ imposto: ", round(custo,2), "custo c/ imposto: ", round(precocusto,2), "preço de venda: ", round(precovenda,2), "preço de venda com frete: ", round(precovendacomfrete,2))
+    vendaproduto=(precovenda*quantidade)
+    precovendacomfrete=(quantidade/frete)
+    print(f"produto: ", produto, "custo s/ imposto: ", round(custo,2), "custo c/ imposto: ", round(precocusto,2), "preço de venda: ", round(precovenda,2), "quantidade: ", round(quantidade), "total da venda: ", round(vendaproduto,2), "preço de venda com frete: ", round(precovendacomfrete,2))
