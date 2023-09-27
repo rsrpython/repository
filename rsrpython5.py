931997509a2f353095464fdef641c05a910c1a67
rsrpython <145908536+rsrpython@users.noreply.github.com>
2023-09-27 17:32:26 -0300

    Delete rsrpython5.py

--- rsrpython5.py
+++
@@ -1,4 +0,0 @@
-percurso=float(input("Digite o percurso a ser percorrido em km: "))
-vel_media=float(input("Digite a velocidade média do veículo: "))
-tempo=float(percurso/vel_media)
-print(f"O tempo necessário para percorrer o percurso é de: ", float(tempo), " hora(s)")
